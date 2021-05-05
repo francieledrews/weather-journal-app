14deddcd57e6155356f67a613263b70c8390baba
Franciele Drews de Souza <francieledrews@gmail.com>
2021-05-05 18:48:30 +0200

    setup APP

--- website/app.js
+++ website/app.js
@@ -1,5 +1,87 @@
 /* Global Variables */
+const form = document.querySelector('.app__form');
+const icons = document.querySelectorAll('.entry__icon');
 
-// Create a new date instance dynamically with JS
+// Base URL and API Key for OpenWeatherMap API
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+let apiKey = '&appid=55489051fc0340323e3a9d1b47dfe5d7';
+
+//Get the date
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', performAction);
+
+/* Function called by event listener */
+function performAction(e) {
+  e.preventDefault();
+  // get user input values
+  const newZip = document.getElementById('zip').value;
+  const content = document.getElementById('feelings').value;
+
+  getWeather(baseURL, newZip, apiKey)
+    .then(function (userData) {
+      // add data to POST request
+      postData('/add', { date: newDate, temp: userData.main.temp, content })
+    }).then(function (newData) {
+      // call updateUI to update browser content
+      updateUI()
+    })
+  // reset form
+  document.getElementById('app-form').reset();
+}
+
+/* Function to GET Web API Data*/
+const getWeather = async (baseURL, newZip, apiKey) => {
+  // res equals to the result of fetch function
+  const res = await fetch(baseURL + newZip + apiKey);
+  try {
+    // userData equals to the result of fetch function
+    const userData = await res.json();
+    return userData;
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
+/* Function to POST data */
+const postData = async (url = '', data = {}) => {
+  const req = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json;charset=UTF-8"
+    },
+    body: JSON.stringify({
+      date: data.date,
+      temp: data.temp,
+      content: data.content
+    })
+  })
+
+  try {
+    const newData = await req.json();
+    return newData;
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
+
+
+const updateUI = async () => {
+  const request = await fetch('/all');
+  try {
+    const allData = await request.json()
+    // show icons on the page
+    icons.forEach(icon => icon.style.opacity = '1');
+    // update new entry values
+    document.getElementById('date').innerHTML = `Date: ${allData.date}`;
+    document.getElementById('temp').innerHTML = `Temperature: ${allData.temp}`;
+    document.getElementById('content').innerHTML = `Today I'm feeling: ${allData.content}`;
+  }
+  catch (error) {
+    console.log("error", error);
+  }
+};
