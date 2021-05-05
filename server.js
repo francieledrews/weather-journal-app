81b0ebab652f89fe70f38641defb388162ea00a0
Franciele Drews de Souza <francieledrews@gmail.com>
2021-05-05 18:47:30 +0200

    GET and POST route

--- server.js
+++ server.js
@@ -1,19 +1,55 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// Require Express to run server and routes
+// Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
+
+/*Dependencies*/
+const bodyParser = require('body-parser');
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+// Spin up the server
+const port = 8080;
+const server = app.listen(port, listening);
 
-// Setup Server
+//Callback to debug
+function listening() {
+    console.log('server running');
+    console.log(`runing on localhost: ${port}`)
+}
+
+
+// Initialize all route with a callback function
+app.get('/all', sendData);
+
+// Callback function to complete GET '/all'
+function sendData(req, res) {
+    res.send(projectData);
+}
+
+//POST route
+const data = [];
+app.post('/add', addData);
+
+function addData(req, res) {
+    projectData['date'] = req.body.date;
+    projectData['temp'] = req.body.temp;
+    projectData['content'] = req.body.content;
+    res.send(projectData);
+}
